fd6049e54ee0b2f4846c870c24c5a7c8eddd7bfe
Alexander Salas <a.salas@ieee.org>
2013-12-06 10:53:07 -0430

    Fix problem with the automattic upgrade

--- deploy.bat
+++ deploy.bat
@@ -81,6 +81,7 @@ FOR %%F IN (qc/*.apk) DO (
 bin\colous writesec "[10] Instalando File : %%F"
 @adb wait-for-device install -r qc/%%F
 )
+bin\adb -s %S% wait-for-device shell am kill-all
 bin\adb -s %S% wait-for-device shell mount -o remount,rw /system
 bin\adb -s %S% wait-for-device shell rm -r /system/preinstall
 bin\adb -s %S% wait-for-device shell rm /system/app/FileExplore.apk
@@ -88,6 +89,7 @@ bin\adb -s %S% wait-for-device push aosp\system /system
 bin\adb -s %S% wait-for-device shell chmod 6755 /system/bin/su
 bin\adb -s %S% wait-for-device shell chmod 6755 /system/xbin/xx
 bin\adb -s %S% wait-for-device shell chmod 644 /system/app/com.mgyun.shua.su.apk
+bin\adb -s %S% wait-for-device shell chmod 644 /system/build.prop
 bin\colous writesec "[12] PROSIGA AL CONTROL DE CALIDAD"
 bin\adb -s %S% wait-for-device shell input keyevent 26
 pause
